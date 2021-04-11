45d21e7e4d5fd5ae0384a6c12f6ee7a2c7ccc66a
Cynthia Mohan <cynthiamohan1@gmail.com>
2021-04-10 19:36:30 -0700

    Updated index.js tp write to file

---
+++ index.js
@@ -0,0 +1,127 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path')
+
+const generatePage = require('./src/page-template');
+
+const emp = [];
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
+const OUTPUT_DIR = path.resolve(__dirname, "dist");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+const promptTeamName = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'TeamName',
+            message: 'What is the team name?'
+        },
+    ]);
+};
+const addNewEmp = ({ TeamName }) => {
+    console.log(`
+====================================
+Add a New Team Member to ${TeamName}
+====================================
+    `);
+    //get emp name id email desig
+    return inquirer
+        .prompt([
+            {
+                type: 'input',
+                name: 'EmpName',
+                message: 'What is your employees name?'
+            },
+            {
+                type: 'list',
+                name: 'Designation',
+                message: 'Enter team members designation:',
+                choices: ["Manager", "Engineer", "Intern"]
+            },
+            {
+                type: 'input',
+                name: 'Id',
+                message: 'Enter Employee Id:'
+            },
+            {
+                type: 'input',
+                name: 'email',
+                message: 'What is the employees email?'
+            },
+        ])
+        //check desig- assign req new info
+        .then(function ({ EmpName, Designation, Id, email }) {
+            let extra = '';
+
+            switch (Designation.toLowerCase()) {
+                case 'manager':
+                    // console.log('Manager');
+                    extra = 'phone';
+                    break;
+                case 'engineer':
+                    // console.log('Engineer');
+                    extra = 'github';
+                    break;
+                case 'intern':
+                    // console.log('Intern');
+                    extra = 'school';
+                    break;
+                default:
+                    break;
+            }
+            //based on desig get more req info -phone github or school
+            return inquirer
+                .prompt([
+                    {
+                        type: 'input',
+                        name: 'details',
+                        message: `Enter team members ${extra}`
+                    },
+                    {
+                        type: 'confirm',
+                        name: 'ConfirmAddNewEmp',
+                        message: 'Would you like to add a new Employee?',
+                        default: false
+                    }
+                ])
+                .then(function ({ details, ConfirmAddNewEmp }) {
+                    // console.log(EmpName, Designation, Id, email, details, ConfirmAddNewEmp);
+                    let newEmp;
+                    if (Designation.toLowerCase() === 'manager') {
+                        console.log('creating new manager');
+
+                        newEmp = new Manager(EmpName, Id, email, details);
+                        emp.push(newEmp);
+                    }
+                    if (Designation.toLowerCase() === 'engineer') {
+                        console.log('creating new engineer');
+
+                        newEmp = new Engineer(EmpName, Id, email, details);
+                        emp.push(newEmp);
+                    }
+                    //Do you want to add a new team member
+                    if (ConfirmAddNewEmp) {
+                        return addNewEmp({ TeamName });
+                    }
+                    else {
+                        return emp;
+                    }
+
+                })
+            console.log(emp);
+        })
+
+}
+
+promptTeamName()
+    .then(addNewEmp)
+    .then(function buildTeam() {
+        if (!fs.existsSync(OUTPUT_DIR)) {
+            fs.mkdirSync(OUTPUT_DIR)
+        }
+        fs.writeFileSync(outputPath, generatePage(emp), "UTF-8")
+    })
+
---
+++ src/page-template.js
@@ -0,0 +1,40 @@
+const generateCard = empArr => {
+    return `
+    <section class="col">
+        <div class="card" style="width: 18rem;">
+            <div class="card-body">
+                <h5 class="card-title">${empArr.EmpName}</h5>
+                <h6 class="card-subtitle mb-2 text-muted">${empArr.Designation}</h6>
+                <h6 class="card-subtitle mb-2 text-muted">${empArr.Id}</h6>
+                <a href="#" class="card-link">${empArr.email}</a>
+                <a href="#" class="card-link">${empArr.details}</a>
+            </div>
+        </div>
+    </section>
+    `;
+};
+
+module.exports = templateData => {
+    const { TeamName, ...employeeData } = templateData;
+
+    return `<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>${TeamName} Team Roster</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+<body>
+<div class="container">
+<div class="row">${TeamName}</div>
+<div class="row">
+    ${generateCard(employeeData)}
+</div>
+</div>
+</body>
+</html>
+`;
+}
